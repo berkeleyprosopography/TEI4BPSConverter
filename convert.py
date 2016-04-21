749bdf38b25bbfafd2aa0a3b759f8c89d5be3a7b
Davide Semenzin <davide@semenz.in>
2016-04-21 04:36:04 -0700

    more like it. lookahead is not hooked up yet, but seems to more or less work. It turned out to be pretty different than what I thought i'd do. It still does most things inefficiently, but it tries to be smart about the content of the table. It operates as such: it does a first pass through the table to get a list of activities, binned by date. Within these sub-tables, it iterates over all the rows that have a role and assume that as a pivot. from there, it first creates the item with its own contents and resolving the relation field. Then it looks ahead to the next lines and interpret those as further information about itself until it finds an element that has a role, at which point it stops.
    '
    '

--- convert.py
+++ convert.py
@@ -141,7 +141,7 @@ def convert(data, size):
 
         log.warn("Found {0} activities in document {1}, ".format( len(activities), str(line[Field.Text_ID])))
 
-        # Traverse the activities list
+        # Traverse the activities list (binned by date, one activity is a sub-list of the main table)
         for uid, activity in enumerate(activities):
 
             data_entry = activities[activity][uid]
@@ -169,74 +169,82 @@ def convert(data, size):
             # this is a person-id lookup dict, will be useful later
             persons_by_sequence = dict((int(x[8]), x) for x in act if x[8])
                 
-            # Here we iterate on every line of the activity (= Person)
+            # Here we iterate on every line of the activity 
             for linenum, line in enumerate(act):
+                print "\n"
+                print "*** Processing document {0} out of {1} ***".format(linenum, len(act))
+                print "--->", linenum, line, '|', uid, activity, '<---'
+                
                 # a convenient try-catch that makes debug easier. 
                 try:
                     # First of all we check that we have a person with a role 
+                    # and if so, we go ahead and create a node for it
                     if line[Field.Role]:
                         pers_name = etree.Element( "persName")
                         pers_name.attrib['role'] = str(line[Field.Role])
                         pers_name.attrib['uid'] = str(line[Field.Person_sequence])
-                    # Otherwise, we just move on
-                    else:
-                        print "BREAK -> PASSING ON", line
-                        pass
-                    
-                    if line[Field.Person_sequence]:
-                        pers_forename = etree.SubElement(pers_name, "forename")
-                        pers_forename.text = clean_ascii(line[Field.Person_name])
-                        
-                        for tag in parse_tags(line[Field.Person_attribute]).items():
-                            elem = etree.SubElement(pers_forename, "state" )
-                            elem.attrib["type"] = str(tag[0])
-                            elem.text = str(tag[1])
 
-                        print "====++++++++++++++========== adding pname ======"
-                        pers_name.append(pers_forename)
-                        perslist.append(pers_name)
-                        print "Added", etree.tostring(pers_name)
-
-                        processed_relations =[]
-
-                        
-                        # Then resolving forenames in person_relation, if any
-                        if line[10]:
-                            tags = parse_tags(line[10])
-                            #print "CHECKPOINT -> resolving tags:", tags
+                        if line[Field.Person_sequence]:
+                            pers_forename = etree.SubElement(pers_name, "forename")
+                            pers_forename.text = clean_ascii(line[Field.Person_name])
                             
-                            for tag in tags.items():
-                                print "iterating over tag", tag
-                                # resolve person name
-                                person = persons_by_sequence[int(tag[1])]
-                                # check if it's a patronymic
-                                if tag[0].lower() == "son":
-                                    pf = etree.SubElement(pers_name, "forename")
-                                    pf.text = clean_ascii(line[Field.Person_name])
-                                    
-                                    for tag in parse_tags(line[Field.Person_attribute]).items():
-                                        elem = etree.SubElement(pf, "state" )
-                                        elem.attrib["type"] = str(tag[0])
-                                        elem.text = str(tag[1])
+                            for tag in parse_tags(line[Field.Person_attribute]).items():
+                                elem = etree.SubElement(pers_forename, "state" )
+                                elem.attrib["type"] = str(tag[0])
+                                elem.text = str(tag[1])
 
-                                    pers_name.append(pf)
-                                    print "CHECKPOINT -> patronym found", tag[0], person[7]
+                            pers_name.append(pers_forename)
+                            perslist.append(pers_name)
+                            print "OK Added new ", etree.tostring(pers_name)
 
-                                else:
-                                    #print "CHECKPOINT -> Found", tag[0], person[7]
-                                    print '                         <forename type="{0}">{1}'.format( tag[0], person[7],)
-                                    for tag in parse_tags(person[9]).items():
-                                    # first resolving attributes
-                                        print '                           <state type="{0}">{1}</state>'.format(tag[0], tag[1])
-                                    print '                         </forename>'
+                            processed_relations =[]
+                            
+                            if line[10]:
+                                tags = parse_tags(line[10])
+                                print "Resolving forenames in person_relation", tags
+                                
+                                for tag in tags.items():
+                                    # resolve person name
+                                    person = persons_by_sequence[int(tag[1])]
+                                    print " Iterating over tag", tag,' ->', person[7]
 
-                                print "=========== tag ==================== adding pname ======"
+                                    print " is {0} a patronymic?".format(tag[0]), tag[0].lower() == "son"
+                                    # check whether it's a patronymic
+                                    if tag[0].lower() == "son":
+                                        
+                                        pf = etree.Element("forename")
+                                        pf.text = clean_ascii(person[Field.Person_name])
+                                        pf.attrib['type'] = "patronymic"
+                                        
+                                        for ptag in parse_tags(line[Field.Person_attribute]).items():
+                                            elem = etree.SubElement(pf, "state" )
+                                            elem.attrib["type"] = str(ptag[0])
+                                            elem.text = str(ptag[1])
+                                        print " A patronymic is its own forename"
+                                        print " [DEBUG] Now pers_name looks like", etree.tostring(pers_name)
+                                        pers_name.append(pf)
+                                        print " CHECKPOINT -> Added", etree.tostring(pf)
+                                        print " [DEBUG] Now pers_name looks like", etree.tostring(pers_name)
 
-                                processed_relations.append(person[8])
-                        else:
-                            print "Person_relation not found, skipping..."
+                                    else:
+                                        pf = etree.Element("state")
+                                        pf.attrib["type"] = str(tag[0])
+                                        pf.text = str(tag[1])
+                                        print " [DEBUG] Now pers_forename looks like", etree.tostring(pers_forename)
+                                        pers_forename.append(pf)
+                                        print " CHECKPOINT -> Added", etree.tostring(pf)
+                                        print " [DEBUG] Now pers_forename looks like", etree.tostring(pers_forename)
+
+                                    print "Done processing", person[8]
+
+                                    processed_relations.append(person[8])
+                            else:
+                                print "Person_relation not found, skipping..."
+                            
+                            print "CHECKPOINT -> LOOKAHEAD to find unbound fliation information"
+
                         
-                        print "CHECKPOINT -> LOOKAHEAD to find unbound fliation information"
+                        
                         loop = True
                         i = 0
                         bucket_list = []
@@ -260,34 +268,15 @@ def convert(data, size):
                         for person in bucket_list:
                             if person[7]:
                                 if person[8] not in processed_relations:
-                                    print '+++++++++UNBOUND', person
-                        
-                        
-                            
-                                
-
+                                    print '+++ UNBOUND', person[9], person[7]
+                                    
                             
                     else:
-                        
-                        print "not person at", line[9]
+                        print "WARNING: not person at", line[9]
                 except Exception as e:
                     print " ************ EXCEPT", str(e)
                     pass
 
-
-                '''
-                for key, value in parse_tags(str(person[Field.Person_attribute])).iteritems():
-                    tag = etree.SubElement(pers_name, "state" )
-                    tag.attrib["type"] = str(key)
-                    tag.text = str(value)
-
-                # This is the content of the "relation" feature
-                if person[Field.Person_relation]:
-                    for key, value in parse_tags(str(person[Field.Person_relation])).iteritems():
-                        tag = etree.SubElement(pers_name, "state" )
-                        tag.attrib["type"] = str(key)
-                        tag.text = str(value)
-                '''
 
 
     log.warn("Ok, conversion complete.")
